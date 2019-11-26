cf1b9f76ac77e765457ffebcf6e6d49295c0d5b6
Paul Mylecharane <hi@public-office.info>
2019-11-27 10:32:56 +1100

    added deadline text

--- site/templates/home.php
+++ site/templates/home.php
@@ -12,7 +12,7 @@
   <div class="text-block">
     <section class="call">
       <h3>CALL FOR PAPERS</h3>
-      <p>INDEX JOURNAL is calling for papers for Issue No. 2 LAW, guest edited by Professor Des Manderson (ANU College of Law) and Professor Ian McLean (Hugh Ramsay Chair of Australian Art History at the University of Melbourne). The editors invite essays and criticism that interrogates the themes of law and justice in works of art, be it in Australian history or the contemporary world. Submit a paper <a style="border-bottom: 1px solid;" href="mailto:editors@index-journal.org">here</a>.<br><br>*Walter Benjamin, The Arcades Project: 476</p>
+      <p>INDEX JOURNAL is calling for papers for Issue No. 2 LAW, guest edited by Professor Des Manderson (ANU College of Law) and Professor Ian McLean (Hugh Ramsay Chair of Australian Art History at the University of Melbourne). The editors invite essays and criticism that interrogates the themes of law and justice in works of art, be it in Australian history or the contemporary world. Deadline for submission is 31 January 2020. <br>Submit a paper <a style="border-bottom: 1px solid;" href="mailto:editors@index-journal.org">here</a>.<br><br>*Walter Benjamin, The Arcades Project: 476</p>
       <p style="text-indent: 0; text-align: center; max-width: 80%; font-size: 90%; margin: 1em auto; position: absolute; bottom: 0.5rem;">Ludwig Hirschfeld-Mack, ‘Desolation (Internment Camp, Orange NSW, 1941)’ <br>woodcut, printed in black ink on thin ivory wove paper, 21.6 x 13.5 cm; Art Gallery of NSW.</p>
     </section>
   </div>
