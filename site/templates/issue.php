8253559878fc74108cfca606411e905f4f81d89a
Paul Mylecharane <hi@public-office.info>
2020-03-11 16:46:38 +1100

    add status: all to issue bp

--- site/templates/issue.php
+++ site/templates/issue.php
@@ -6,22 +6,12 @@
       <div class="issue-items">
         <ul>
           <? if($page->hasChildren()): ?>
-            <? foreach($page->children()->visible() as $article): ?>
+            <? foreach($page->children()->listed() as $article): ?>
               <li><a href="<?= $article->url() ?>"><span class="title"><?= $article->title() ?></span> by <?= $article->author() ?></a></li>
             <? endforeach ?>
           <? endif ?>
         </ul>
       </div>
-      <!-- <h1>Book Reviews</h1>
-      <div class="issue-items">
-        <ul>
-          <? if($page->hasChildren()): ?>
-            <? foreach($page->children()->visible() as $article): ?>
-              <li><a href="<?= $article->url() ?>"><span class="title"><?= $article->title() ?></span> by <?= $article->author() ?></a></li>
-            <? endforeach ?>
-          <? endif ?>
-        </ul>
-      </div> -->
     </section>
   </main>
 </body>
