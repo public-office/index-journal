f09825f47a1600a3ebf27d4f9e8a8dec4818914b
Matt Lenz <matt@mattlenz.com>
2020-03-11 14:53:56 +1100

    Test generate print pdf

--- site/config/config.php
+++ site/config/config.php
@@ -1,10 +1,24 @@
 <?
+function output_pdf($page) {
+  if($page->template() == 'essay'):
+    $pdf_name = $page->slug().'.pdf';
+    $outfile = $page->contentFileDirectory().'/'.$pdf_name;
+    $url = $page->url();
+
+    exec('google-chrome --headless --print-to-pdf="'.$outfile.'" '.$url. ' > /dev/null 2>/dev/null &');
+  endif;
+}
 
 return [
   'debug' => true,
   'markdown' => [
     'extra' => true
   ],
-  'panel.install' => true
+  'panel.install' => true,
+  'hooks' => [
+    'page.update:after' => function($page, $oldPage) {
+      output_pdf($page);
+    }
+  ]
 ];
 ?>
