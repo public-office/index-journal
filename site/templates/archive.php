21265059272cf50b3201b0014d39b42eb2abb54e
Paul Mylecharane <hi@public-office.info>
2020-03-11 13:53:58 +1100

    setting up print styles

---
+++ site/templates/archive.php
@@ -0,0 +1,9 @@
+<? snippet('header') ?>
+
+  <main data-template="<?= $page->template() ?>">
+    <? foreach(page('issues')->children()->listed()->filterBy('issue_type', 'archive') as $issue): ?>
+      <?= $issue->title() ?>
+    <? endforeach ?>
+  </main>
+</body>
+</html>
