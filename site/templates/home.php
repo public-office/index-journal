ca544a8845b0596f46625c7f56eab6173a9f1d0d
Paul Mylecharane <hi@public-office.info>
2020-03-13 16:30:45 +1100

    fixing overflow on bibliography

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,8 +1,8 @@
 <? snippet('header') ?>
 
 <main>
-  <? foreach(page('issues')->children()->listed()->flip()->limit(1) as $home): ?>
-    <? $sections = $home->cfp_builder()->toStructure() ?>
+
+    <? $sections = $page->cfp_builder()->toStructure() ?>
     <? foreach($sections as $section): ?>
       <section class="initial">
         <?= $section->message()->kirbytext() ?>
@@ -14,28 +14,13 @@
       <div class="text-block">
         <section class="call">
           <?= $section->secondary_text()->kirbytext() ?>
-
-          <section class="title-block">
-            <span class="title-block-issue">Issue No.<?= $home->num() ?> <span class="title"><?= $home->title() ?></span></span>
-            <div class="issue-items">
-              <ul>
-                <? if($home->hasChildren()): ?>
-                  <? foreach($home->children()->listed() as $article): ?>
-                    <li><a href="<?= $article->url() ?>"><span class="title"><?= $article->title() ?></span> by <?= $article->author() ?></a></li>
-                  <? endforeach ?>
-                <? endif ?>
-              </ul>
-            </div>
-          </section>
           <p style="text-indent: 0; text-align: center; max-width: 80%; font-size: 90%; margin: 1em auto; position: absolute; bottom: 0.5rem;"><?= $section->wipe_img_caption() ?></p>
         </section>
       </div>
+
     <? endforeach ?>
-    <div class="issue-overlay">
-      <span class="issue-overlay-close">(close)</span>
-      <img src="<?= $home->issue_image()->toFile()->url() ?>" alt="">
-    </div>
-  <? endforeach ?>
+
+
 </main>
 </body>
 </html>
