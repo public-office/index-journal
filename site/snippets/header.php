6f321a672a9fd5e1bd25667765f2f8ad055c3675
parislettau <55466163+parislettau@users.noreply.github.com>
2021-11-25 09:15:40 +1100

    Update header.php

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -21,5 +21,5 @@
   </head>
   <body data-display="<?= $page->template() ?>">
     <header <? if($page->template() == 'essay'): ?>style="background-color: rgb(<?= $page->parent()->issue_color() ?>); box-shadow: 0px 11px 16px 0px rgba(<?= $page->parent()->issue_color() ?>,1);"<? endif ?>>
-      <h1><a href="<?= $site->url() ?>">INDEX JOURNAL</a>, <nav><?foreach(page('issues')->children()->listed()->flip() as $issue): ?><span class="issue"><span><a href="<?= $issue->url() ?>"><span class="hide-mobile">Issue</span> <span>No.</span> <?= $issue->num() ?> <span class="title"><?= $issue->title() ?></span></a></span><? endforeach ?></span></nav></h1><nav> <a href="<?= $site->url() ?>/about">ABOUT</a><? if(page('emaj')->isListed()): ?>, <a href="<?= $site->url() ?>/emaj"><span>emaj</span> ARCHIVE</a><? endif ?></nav>
+      <h1><a href="<?= $site->url() ?>">INDEX JOURNAL~</a>, <nav><?foreach(page('issues')->children()->listed()->flip() as $issue): ?><span class="issue"><span><a href="<?= $issue->url() ?>"><span class="hide-mobile">Issue</span> <span>No.</span> <?= $issue->num() ?> <span class="title"><?= $issue->title() ?></span></a></span><? endforeach ?></span></nav></h1><nav> <a href="<?= $site->url() ?>/about">ABOUT</a><? if(page('emaj')->isListed()): ?>, <a href="<?= $site->url() ?>/emaj"><span>emaj</span> ARCHIVE</a><? endif ?></nav>
     </header>
