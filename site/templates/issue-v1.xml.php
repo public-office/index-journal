10601c767999e97ea0ed5db4f1f2e75bbd67252a
parislettau <paris.lettau@gmail.com>
2021-12-07 11:08:27 +1100

    xml

---
+++ site/templates/issue-v1.xml.php
@@ -0,0 +1,120 @@
+<doi_batch xmlns="http://www.crossref.org/schema/4.4.2" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" version="4.4.2" xsi:schemaLocation="http://www.crossref.org/schema/4.4.2 http://www.crossref.org/schema/deposit/crossref4.4.2.xsd">
+    <head>
+        <doi_batch_id>index-journal-<?php echo date("YmdHis") ?></doi_batch_id>
+        <timestamp><?php echo date("YmdHis") ?></timestamp>
+        <depositor>
+            <depositor_name>indj</depositor_name>
+            <email_address>editors@index-journal.org</email_address>
+        </depositor>
+        <registrant>WEB-FORM</registrant>
+    </head>
+
+    <body>
+        <journal>
+            <journal_metadata>
+                <full_title>Index Journal</full_title>
+                <abbrev_title>Index</abbrev_title>
+                <issn media_type="electronic">26524740</issn>
+                <doi_data>
+                    <doi>10.38030/index-journal</doi>
+                    <timestamp><?php echo date("YmdHis") ?></timestamp>
+                    <resource>http://www.index-journal.org/</resource>
+                </doi_data>
+            </journal_metadata>
+
+            <journal_issue>
+                <publication_date media_type="print">
+                    <year><?= $page->issue_date()->toDate('Y') ?></year>
+                </publication_date>
+                <publication_date media_type="online">
+                    <year><?= $page->issue_date()->toDate('Y') ?></year>
+                </publication_date>
+                <issue><?= $page->issue_num() ?></issue>
+                <doi_data>
+                    <doi><?= $page->issue_doi() ?></doi>
+                    <resource><? $page->url() ?></resource>
+                </doi_data>
+            </journal_issue>
+            
+            <?php foreach ($page->children()->listed() as $subPage) : ?>
+                <?php if ($subPage->hasChildren()) : ?>
+                    <?php foreach ($subPage->children()->listed() as $essay) : ?>
+                        <journal_article publication_type="full_text">
+                            <titles>
+                                <title><?= $essay->title() ?><?php if ($essay->subtitle()->isNotEmpty()) : ?>: <?= $essay->subtitle() ?><?php endif ?></title>
+                            </titles>
+                            <contributors>
+                                <?php foreach ($essay->authors()->toStructure()->slice(0, 1) as $author) : ?>
+                                    <person_name sequence="first" contributor_role="author">
+                                        <given_name><?= $author->first_name() ?></given_name>
+                                        <surname><?= $author->last_name() ?></surname>
+                                        <?php if ($author->orcid()->isNotEmpty()) : ?>
+                                            <ORCID><?= $author->orcid() ?></ORCID>
+                                        <?php endif ?>
+                                    </person_name>
+                                <?php endforeach ?>
+                                <?php foreach ($essay->authors()->toStructure()->slice(1) as $author) : ?>
+                                    <person_name sequence="additional" contributor_role="author">
+                                        <given_name><?= $author->first_name() ?></given_name>
+                                        <surname><?= $author->last_name() ?></surname>
+                                        <?php if ($author->orcid()->isNotEmpty()) : ?>
+                                            <ORCID><?= $author->orcid() ?></ORCID>
+                                        <?php endif ?>
+                                    </person_name>
+                                <?php endforeach ?>
+                            </contributors>
+                            <publication_date media_type="print">
+                                <year><?= $page->issue_date()->toDate('Y') ?></year>
+                            </publication_date>
+                            <publication_date media_type="online">
+                                <year><?= $page->issue_date()->toDate('Y') ?></year>
+                            </publication_date>
+                            <doi_data>
+                                <doi><?= $essay->doi() ?></doi>
+                                <resource><?= $essay->url() ?></resource>
+                            </doi_data>
+                        </journal_article>
+                    <?php endforeach ?>
+                <?php else :
+                    $essay = $subPage
+                ?>
+                    <journal_article publication_type="full_text">
+                        <titles>
+                            <title><?= $subPage->title() ?></title>
+                        </titles>
+                        <contributors>
+                            <?php foreach ($essay->authors()->toStructure()->slice(0,1) as $author) : ?>
+                                <person_name sequence="first" contributor_role="author">
+                                    <given_name><?= $author->first_name() ?></given_name>
+                                    <surname><?= $author->last_name() ?></surname>
+                                    <?php if ($author->orcid()->isNotEmpty()) : ?>
+                                        <ORCID><?= $author->orcid() ?></ORCID>
+                                    <?php endif ?>
+                                </person_name>
+                            <?php endforeach ?>
+                            <?php foreach ($essay->authors()->toStructure()->slice(1) as $author) : ?>
+                                <person_name sequence="additional" contributor_role="author">
+                                    <given_name><?= $author->first_name() ?></given_name>
+                                    <surname><?= $author->last_name() ?></surname>
+                                    <?php if ($author->orcid()->isNotEmpty()) : ?>
+                                        <ORCID><?= $author->orcid() ?></ORCID>
+                                    <?php endif ?>
+                                </person_name>
+                            <?php endforeach ?>
+                        </contributors>
+                        <publication_date media_type="print">
+                            <year><?= $page->issue_date()->toDate('Y') ?></year>
+                        </publication_date>
+                        <publication_date media_type="online">
+                            <year><?= $page->issue_date()->toDate('Y') ?></year>
+                        </publication_date>
+                        <doi_data>
+                            <doi><?= $essay->doi() ?></doi>
+                            <resource><?= $essay->url() ?></resource>
+                        </doi_data>
+                    </journal_article>
+                <?php endif ?>
+            <?php endforeach ?>
+        </journal>
+    </body>
+</doi_batch>
--- site/templates/issue.xml.php
+++ site/templates/issue.xml.php
@@ -1,117 +1,82 @@
-<doi_batch xmlns="http://www.crossref.org/schema/4.4.2" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" version="4.4.2" xsi:schemaLocation="http://www.crossref.org/schema/4.4.2 http://www.crossref.org/schema/deposit/crossref4.4.2.xsd">
+<?xml version="1.0" encoding="UTF-8"?>
+<doi_batch xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xsi:schemaLocation="http://www.crossref.org/schema/5.3.0 ../../schemas/crossref5.3.0.xsd"
+    xmlns="http://www.crossref.org/schema/5.3.0" xmlns:jats="http://www.ncbi.nlm.nih.gov/JATS1"
+    xmlns:fr="http://www.crossref.org/fundref.xsd" version="5.3.0" xmlns:mml="http://www.w3.org/1998/Math/MathML">
     <head>
         <doi_batch_id>index-journal-<?php echo date("YmdHis") ?></doi_batch_id>
-        <timestamp><?php echo date("YmdHis") ?></timestamp>
+        <timestamp>2021010100000000</timestamp>
         <depositor>
             <depositor_name>indj</depositor_name>
             <email_address>editors@index-journal.org</email_address>
         </depositor>
-        <registrant>WEB-FORM</registrant>
+        <!-- <registrant>Society of Metadata Idealists</registrant> -->
     </head>
-
     <body>
         <journal>
-            <journal_metadata>
+            <journal_metadata language="en" reference_distribution_opts="any">
                 <full_title>Index Journal</full_title>
                 <abbrev_title>Index</abbrev_title>
-                <issn media_type="electronic">26524740</issn>
+                <issn media_type="electronic">2652-4740</issn>
+                <!-- <coden>xyIUB8</coden> -->
+                <!-- <archive_locations>
+                    <archive name="Portico"/>
+                    <archive name="CLOCKSS"/>
+                    <archive name="DWT"/>
+                    <archive name="Internet Archive"/>
+                    <archive name="KB"/>
+                    <archive name="LOCKSS"/>
+                </archive_locations> -->
                 <doi_data>
                     <doi>10.38030/index-journal</doi>
+                    <timestamp><?php echo date("YmdHis") ?></timestamp>
                     <resource>http://www.index-journal.org/</resource>
                 </doi_data>
             </journal_metadata>
+
             <journal_issue>
-                <publication_date media_type="print">
-                    <year><?= $page->issue_date()->toDate('Y') ?></year>
-                </publication_date>
+                <!-- contributors and title are mostly relevant for special issues that have guest editors -->
+                <contributors>
+                    <?php foreach ($page->editors()->toStructure()->slice(0, 1) as $editor) : ?>
+                        <person_name sequence="first" contributor_role="editor">
+                            <given_name><?= $editor->first_name() ?></given_name>
+                            <surname><?= $editor->last_name() ?></surname>
+                            <?php if ($editor->orcid()->isNotEmpty()) : ?>
+                                <ORCID><?= $editor->orcid() ?></ORCID>
+                            <?php endif ?>
+                        </person_name>
+                    <?php endforeach ?>
+                    <?php foreach ($page->editors()->toStructure()->slice(1) as $editor) : ?>
+                        <person_name sequence="additional" contributor_role="editor">
+                            <given_name><?= $editor->first_name() ?></given_name>
+                            <surname><?= $editor->last_name() ?></surname>
+                            <?php if ($editor->orcid()->isNotEmpty()) : ?>
+                                <ORCID><?= $editor->orcid() ?></ORCID>
+                            <?php endif ?>
+                        </person_name>
+                    <?php endforeach ?>
+                </contributors>
+                <titles>
+                    <title><?php $page->title() ?></title> 
+                </titles>
                 <publication_date media_type="online">
-                    <year><?= $page->issue_date()->toDate('Y') ?></year>
+                    <month>07</month>
+                    <day>07</day>
+                    <year>2021</year>
                 </publication_date>
-                <issue><?= $page->issue_num() ?></issue>
+                <journal_volume>
+                    <volume>4</volume>
+                    <doi_data>
+                        <doi>10.32013/U.4fAZy4</doi>
+                        <resource>https://gitlab.com/crossref/schema</resource>
+                    </doi_data>
+                </journal_volume>
+                <issue>1</issue>
                 <doi_data>
-                    <doi><?= $page->issue_doi() ?></doi>
-                    <resource><? $page->url() ?></resource>
+                    <doi>10.32013/B2Y7qLJ</doi>
+                    <resource>https://gitlab.com/crossref/schema</resource>
                 </doi_data>
             </journal_issue>
-            <?php foreach ($page->children()->listed() as $subPage) : ?>
-                <?php if ($subPage->hasChildren()) : ?>
-                    <?php foreach ($subPage->children()->listed() as $essay) : ?>
-                        <journal_article publication_type="full_text">
-                            <titles>
-                                <title><?= $essay->title() ?><?php if ($essay->subtitle()->isNotEmpty()) : ?>: <?= $essay->subtitle() ?><?php endif ?></title>
-                            </titles>
-                            <contributors>
-                                <?php foreach ($essay->authors()->toStructure()->slice(0, 1) as $author) : ?>
-                                    <person_name sequence="first" contributor_role="author">
-                                        <given_name><?= $author->first_name() ?></given_name>
-                                        <surname><?= $author->last_name() ?></surname>
-                                        <?php if ($author->orcid()->isNotEmpty()) : ?>
-                                            <ORCID><?= $author->orcid() ?></ORCID>
-                                        <?php endif ?>
-                                    </person_name>
-                                <?php endforeach ?>
-                                <?php foreach ($essay->authors()->toStructure()->slice(1) as $author) : ?>
-                                    <person_name sequence="additional" contributor_role="author">
-                                        <given_name><?= $author->first_name() ?></given_name>
-                                        <surname><?= $author->last_name() ?></surname>
-                                        <?php if ($author->orcid()->isNotEmpty()) : ?>
-                                            <ORCID><?= $author->orcid() ?></ORCID>
-                                        <?php endif ?>
-                                    </person_name>
-                                <?php endforeach ?>
-                            </contributors>
-                            <publication_date media_type="print">
-                                <year><?= $page->issue_date()->toDate('Y') ?></year>
-                            </publication_date>
-                            <publication_date media_type="online">
-                                <year><?= $page->issue_date()->toDate('Y') ?></year>
-                            </publication_date>
-                            <doi_data>
-                                <doi><?= $essay->doi() ?></doi>
-                                <resource><?= $essay->url() ?></resource>
-                            </doi_data>
-                        </journal_article>
-                    <?php endforeach ?>
-                <?php else :
-                    $essay = $subPage
-                ?>
-                    <journal_article publication_type="full_text">
-                        <titles>
-                            <title><?= $subPage->title() ?></title>
-                        </titles>
-                        <contributors>
-                            <?php foreach ($essay->authors()->toStructure()->slice(0,1) as $author) : ?>
-                                <person_name sequence="first" contributor_role="author">
-                                    <given_name><?= $author->first_name() ?></given_name>
-                                    <surname><?= $author->last_name() ?></surname>
-                                    <?php if ($author->orcid()->isNotEmpty()) : ?>
-                                        <ORCID><?= $author->orcid() ?></ORCID>
-                                    <?php endif ?>
-                                </person_name>
-                            <?php endforeach ?>
-                            <?php foreach ($essay->authors()->toStructure()->slice(1) as $author) : ?>
-                                <person_name sequence="additional" contributor_role="author">
-                                    <given_name><?= $author->first_name() ?></given_name>
-                                    <surname><?= $author->last_name() ?></surname>
-                                    <?php if ($author->orcid()->isNotEmpty()) : ?>
-                                        <ORCID><?= $author->orcid() ?></ORCID>
-                                    <?php endif ?>
-                                </person_name>
-                            <?php endforeach ?>
-                        </contributors>
-                        <publication_date media_type="print">
-                            <year><?= $page->issue_date()->toDate('Y') ?></year>
-                        </publication_date>
-                        <publication_date media_type="online">
-                            <year><?= $page->issue_date()->toDate('Y') ?></year>
-                        </publication_date>
-                        <doi_data>
-                            <doi><?= $essay->doi() ?></doi>
-                            <resource><?= $essay->url() ?></resource>
-                        </doi_data>
-                    </journal_article>
-                <?php endif ?>
-            <?php endforeach ?>
-        </journal>
+        </journal>        
     </body>
 </doi_batch>
