bc66afc436ddb814ba60dc461984f5fb7f29f6ea
parislettau <paris.lettau@gmail.com>
2021-12-07 12:12:01 +1100

    xml

--- site/templates/issue.xml.php
+++ site/templates/issue.xml.php
@@ -1,4 +1,5 @@
 <doi_batch xmlns="http://www.crossref.org/schema/4.4.2" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" version="4.4.2" xsi:schemaLocation="http://www.crossref.org/schema/4.4.2 http://www.crossref.org/schema/deposit/crossref4.4.2.xsd">
+
     <head>
         <doi_batch_id>index-journal-<?php echo date("YmdHis") ?></doi_batch_id>
         <timestamp><?php echo date("YmdHis") ?></timestamp>
@@ -21,10 +22,32 @@
                 </doi_data>
             </journal_metadata>
             <journal_issue>
-                <publication_date media_type="print">
-                    <year><?= $page->issue_date()->toDate('Y') ?></year>
-                </publication_date>
+                <contributors>
+                    <?php foreach ($page->editors()->toStructure()->slice(0, 1) as $editor) : ?>
+                        <person_name sequence="first" contributor_role="editor">
+                            <given_name><?= $editor->first_name() ?></given_name>
+                            <surname><?= $editor->last_name() ?></surname>
+                            <?php if ($editor->orcid()->isNotEmpty()) : ?>
+                                <ORCID><?= $editor->orcid() ?></ORCID>
+                            <?php endif ?>
+                        </person_name>
+                    <?php endforeach ?>
+                    <?php foreach ($page->editors()->toStructure()->slice(1) as $editor) : ?>
+                        <person_name sequence="additional" contributor_role="editor">
+                            <given_name><?= $editor->first_name() ?></given_name>
+                            <surname><?= $editor->last_name() ?></surname>
+                            <?php if ($editor->orcid()->isNotEmpty()) : ?>
+                                <ORCID><?= $editor->orcid() ?></ORCID>
+                            <?php endif ?>
+                        </person_name>
+                    <?php endforeach ?>
+                </contributors>
+                <titles>
+                    <title><?php $page->title() ?></title>
+                </titles>
                 <publication_date media_type="online">
+                    <month><?= $page->issue_date()->toDate('m') ?></month>
+                    <day><?= $page->issue_date()->toDate('d') ?></day>
                     <year><?= $page->issue_date()->toDate('Y') ?></year>
                 </publication_date>
                 <issue><?= $page->issue_num() ?></issue>
@@ -80,7 +103,7 @@
                             <title><?= $subPage->title() ?></title>
                         </titles>
                         <contributors>
-                            <?php foreach ($essay->authors()->toStructure()->slice(0,1) as $author) : ?>
+                            <?php foreach ($essay->authors()->toStructure()->slice(0, 1) as $author) : ?>
                                 <person_name sequence="first" contributor_role="author">
                                     <given_name><?= $author->first_name() ?></given_name>
                                     <surname><?= $author->last_name() ?></surname>
