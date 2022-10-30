58e8debefc892786c76bbb5a69931039082f66de
parislettau <55466163+parislettau@users.noreply.github.com>
2022-10-31 08:50:20 +1100

    scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,27 +1,29 @@
 #!/usr/bin/env sh
-PROJECT=~/sites/index-journal
-
 # Ask the user for a descripion of push
 echo Hello, please enter short description of the push:
 read varname
 
-DESCRIPTION=$varname
+# get all the variables
+DESCRIPTION=$varname          # get the description from user input
+PROJECT=${PWD##*/}            # get project directory name
+DIRECTORY=~/sites/$PROJECT    # get the local directory of the project
 
-cd $PROJECT
+# add to git
+cd $DIRECTORY
 git pull
 git add .
 git commit -m "$DESCRIPTION"
 git push
 
-USER=root
-HOST=149.28.168.105
-WEBROOT=/var/www/index-journal
+# pull to server
+USER=root                     # connect with root user
+HOST=149.28.168.105           # 
+WEBROOT=/var/www/$PROJECT     # get the location of the project on the server
 
 ssh -A $USER@$HOST /bin/bash <<EOF
   cd $WEBROOT
   git pull
   sudo chown -R www-data:www-data .
 EOF
-
 
 
--- scripts/pull.sh
+++ scripts/pull.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env sh
 
-<<<<<<< Updated upstream
-=======
-LOCAL=~/Sites/index-journal
-
-cd $LOCAL
->>>>>>> Stashed changes
+PROJECT=${PWD##*/}  # get project directory name
 HOST=root@149.28.168.105
-WEBROOT=/var/www/index-journal
+WEBROOT=/var/www/$PROJECT
 
 rsync -r -p -t -u -z --checksum --exclude=".*" -P -h -i --delete $HOST:$WEBROOT/content ./
+
