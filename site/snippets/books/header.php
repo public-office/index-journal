b865b2ae79569b5aabcb2771c5b5b81ad4a1bc4f
Paris Lettau <55466163+parislettau@users.noreply.github.com>
2022-11-29 08:35:55 +1100

    matomo

--- site/snippets/books/header.php
+++ site/snippets/books/header.php
@@ -30,6 +30,25 @@
     gtag('config', 'UA-139982363-1');
   </script>
   <script src="https://unpkg.com/loadeer" defer init></script>
+  <!-- Matomo -->
+  <script>
+    var _paq = window._paq = window._paq || [];
+    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function() {
+      var u = "//matomo.memoreview.net/";
+      _paq.push(['setTrackerUrl', u + 'matomo.php']);
+      _paq.push(['setSiteId', '3']);
+      var d = document,
+        g = d.createElement('script'),
+        s = d.getElementsByTagName('script')[0];
+      g.async = true;
+      g.src = u + 'matomo.js';
+      s.parentNode.insertBefore(g, s);
+    })();
+  </script>
+  <!-- End Matomo Code -->
 </head>
 
 <body data-display="<?= $page->template() ?>">
--- site/snippets/header.php
+++ site/snippets/header.php
@@ -24,6 +24,25 @@
     gtag('js', new Date());
     gtag('config', 'UA-139982363-1');
   </script>
+  <!-- Matomo -->
+  <script>
+    var _paq = window._paq = window._paq || [];
+    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function() {
+      var u = "//matomo.memoreview.net/";
+      _paq.push(['setTrackerUrl', u + 'matomo.php']);
+      _paq.push(['setSiteId', '3']);
+      var d = document,
+        g = d.createElement('script'),
+        s = d.getElementsByTagName('script')[0];
+      g.async = true;
+      g.src = u + 'matomo.js';
+      s.parentNode.insertBefore(g, s);
+    })();
+  </script>
+  <!-- End Matomo Code -->
 </head>
 
 <body data-display="<?= $page->template() ?>">
