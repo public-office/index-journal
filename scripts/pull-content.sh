3cb72792807cdc7989dba7494c6535afdf9d684a
parislettau <55466163+parislettau@users.noreply.github.com>
2024-09-05 14:02:25 +1000

    fix width

--- scripts/pull-content.sh
+++ scripts/pull-content.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # PROJECT=${PWD##*/}  # get project directory name
-HOST=ubuntu@67.219.98.22
+HOST=ubuntu@45.32.247.37
 WEBROOT=/etc/easypanel/projects/memo/index-journal/volumes
 
 rsync -r -p -t -u -z --checksum --exclude=".*" -P -h -i --delete $HOST:$WEBROOT/content ./
