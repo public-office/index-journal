775da4b592209f22925f9f74231da064fb3be634
parislettau <55466163+parislettau@users.noreply.github.com>
2024-09-06 13:44:55 +1000

    Fix essay elemetns

--- site/templates/essay.php
+++ site/templates/essay.php
@@ -1,39 +1,55 @@
 <?php snippet('header') ?>
 
-
 <main data-template="<?= $page->template() ?>">
-
+  <!-- Title -->
   <section class="title-block" style="background-color: rgb(<?= $page->parent()->issue_color() ?>)">
-    <h1 style="font-size:6vw"><span class="title"><?= $page->title() ?></span>
-      <?php if ($page->subtitle()->isNotEmpty()) : ?><span class="subtitle"><?= smartypants($page->subtitle()->kti()) ?></span><?php endif ?>
-      <?php if ($page->slug() != 'introduction') : ?><span class="author">by <?= $page->author() ?></span><?php endif ?>
+    <h1 style="font-size:6vw">
+      <span class="title"><?= $page->title() ?></span>
+      <?php if ($page->subtitle()->isNotEmpty()) : ?>
+        <span class="subtitle"><?= smartypants($page->subtitle()->kti()) ?></span>
+      <?php endif ?>
+      <?php if ($page->slug() != 'introduction') : ?>
+        <span class="author">by <?= $page->author() ?></span>
+      <?php endif ?>
     </h1>
   </section>
+
+  <!-- Text -->
   <div class="text-block">
-    <?php if ($page->slug() != 'introduction') : ?><?php if ($page->hasDocuments()) : ?><span class="pdf">(<a href="<?= $page->documents()->first()->url() ?>" target="_blank" rel="noopener noreferrer">PDF</a>)</span><?php endif ?></span><?php endif ?>
-<?php if ($page->abstract()->isNotEmpty()) : ?>
-  <div class="abstract-block">
-    <div class="abstract-content">
-      <div class="shadow"></div>
-      <div class="abstract hidden"><?= smartypants($page->abstract()->kirbytext()) ?></div>
+    <?php if ($page->slug() != 'introduction') : ?>
+      <?php if ($page->hasDocuments()) : ?>
+        <span class="pdf">(<a href="<?= $page->documents()->first()->url() ?>" target="_blank" rel="noopener noreferrer">PDF</a>)</span>
+      <?php endif ?>
+    <?php endif ?>
+    <!-- content -->
+    <div class="content-block">
+      <?= smartypants($page->text()->kirbytext()) ?>
     </div>
   </div>
-<?php endif ?>
 
-<?= smartypants($page->text()->kirbytext()) ?>
-
-<div class="text-bios">
-  <?= smartypants($page->bios()->kirbytext()) ?>
-</div>
-
-<?php if ($page->bibilography()->isNotEmpty()) : ?>
-  <p style="font-size: var(--font-small); text-indent: 0; text-transform: uppercase; margin-bottom: 1em;">Bibliography</p>
-  <div class="text-bibliography">
-    <?= smartypants($page->bibilography()->kirbytext()) ?>
+  <!-- Bios -->
+  <div class="text-bios">
+    <?= smartypants($page->bios()->kirbytext()) ?>
   </div>
-<?php endif ?>
-  </div>
-  <?php if ($page->slug() != 'introduction') : ?><span class="essay-extra">(<?php if ($page->doi()->isNotEmpty()) : ?><span class="doi"><a href="http://doi.org/<?= $page->doi() ?>"><?= $page->doi() ?></a>)</span><?php endif ?><?php endif ?>
+
+  <!-- bibliographies -->
+  <?php if ($page->bibilography()->isNotEmpty()) : ?>
+    <p style="font-size: var(--font-small); text-indent: 0; text-transform: uppercase; margin-bottom: 1em; padding: 0 4em;
+">Bibliography</p>
+    <div class="text-bibliography">
+      <?= smartypants($page->bibilography()->kirbytext()) ?>
+    </div>
+  <?php endif ?>
+
+  <?php if ($page->slug() != 'introduction') : ?>
+    <span class="essay-extra">
+      <?php if ($page->doi()->isNotEmpty()) : ?>
+        <span class="doi">
+          <a href="http://doi.org/<?= $page->doi() ?>"><?= $page->doi() ?></a>
+        </span>
+      <?php endif ?>
+    </span>
+  <?php endif ?>
 </main>
 </body>
 
