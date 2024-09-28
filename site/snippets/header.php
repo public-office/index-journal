e87bfc52dd0664a4226100f843acf090b4086c28
parislettau <55466163+parislettau@users.noreply.github.com>
2024-09-28 12:51:32 +1000

    Update header.php

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -4,7 +4,6 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>INDEX JOURNAL</title>
   <?php snippet('meta_information'); ?>
   <?php snippet('robots'); ?>
 
